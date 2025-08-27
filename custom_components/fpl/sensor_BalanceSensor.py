5f4eef7d04d13d04b0a5a12462e3d47dd0fbe032
Camilo Valderruten <cvalderruten@nerdwallet.com>
2025-08-27 10:44:04 -0400

    Fix the BalanceSensor

--- custom_components/fpl/sensor_BalanceSensor.py
+++ custom_components/fpl/sensor_BalanceSensor.py
@@ -10,24 +10,11 @@ class BalanceSensor(FplMoneyEntity):
 
     @property
     def native_value(self):
-        data = self.getData("balance_data")
-
-        self._attr_native_value = 0
-
-        if data is not None and len(data) > 0 and "amount" in data[0].keys():
-            self._attr_native_value = data[0]["amount"]
-
+        self._attr_native_value = self.getData("balance")
         return self._attr_native_value
 
     def customAttributes(self):
-        """Return the state attributes."""
-        attributes = {}
-        data = self.getData("balance_data")
-
-        if data is not None and len(data) > 0:
-            if "details" in data[0].keys():
-                attributes["details"] = data[0]["details"]
-            if "dueDate" in data[0].keys():
-                attributes["dueDate"] = data[0]["dueDate"]
-
+        attributes = {
+            "pastDue": self.getData("pastDue")
+        }
         return attributes
